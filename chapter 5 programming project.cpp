42bceab889de00fa5f32d3e9bdebc03cd2d2f46f
careybarberjr@gmail.com <careybarberjr@gmail.com>
2024-11-14 21:33:21 -0500

    q

--- chapter 5 programming project.cpp
+++ chapter 5 programming project.cpp
@@ -2,11 +2,31 @@
 //
 
 #include <iostream>
+#include <string>
+#include <fstream>
 using namespace std;
+
 
 int main()
 {
-    cout << "Hello World!\n";
 
+
+    ifstream inputFile;
+    inputFile.open("LineUp.txt");
+    
+    string name1 = "jackie\n";
+    string name2 = "sam\n";
+    string name3 = "tom\n";
+    string name4 = "bill\n";
+    string name5 = "mary\n";
+    string name6 = "paul\n";
+    string name7 = "zev\n";
+    string name8 = "berb\n";
+
+    cout << "there is a total of 8 students in the class" << endl;
+
+    cout << "the students to be lined up in alphabetical order are as follows:\n" << name8 << name4 << name1 << name5 << name6 << name2 << name3 << name7;
+    
+    inputFile.close();
 }
 
