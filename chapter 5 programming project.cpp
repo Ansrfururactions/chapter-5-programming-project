1fa037b0852b0c05cd9381830ce813def40d7e96
careybarberjr@gmail.com <careybarberjr@gmail.com>
2024-12-02 10:04:02 -0500

    q

--- chapter 5 programming project.cpp
+++ chapter 5 programming project.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
     int organism = 0, days = 0, amount=0, day = 1;
+    double GrowthRate = 0.0;
 
     while (organism < 2) 
     {
@@ -14,6 +15,11 @@ int main()
             cout << "the number of organisms given is below the minimum, please enter at least two" << endl;
         }
     }
+
+    cout << "enter the average growth rate of the population (represent it as a percentage)";
+        cin >> GrowthRate;
+        GrowthRate = GrowthRate;
+
     while (days <= 1)
     {
         cout << "enter the amount of days for them to multiply" << endl;
@@ -25,7 +31,7 @@ int main()
     }
     while (day <= days)
     {
-        amount = organism * day;
+        organism = organism + organism*GrowthRate;
         cout << "the amount of organisms that would be produced on day " << day << " would be " << amount << endl;
         day++;
     }
